1d3c93012ed846ff9de459e16c88d6baf47ac0d0
shashankgarewal <shashankgarewal4+github@gmail.com>
2026-05-09 20:04:38 +0530

    feat: implement tuning, evalutate, and build model components

--- src/model/train_churn.py
+++ src/model/train_churn.py
@@ -1,11 +1,9 @@
 import pandas as pd
 from pathlib import Path
-from sklearn.ensemble import (
-    RandomForestClassifier,
-    HistGradientBoostingClassifier,
-    ExtraTreesClassifier,
-)
+from sklearn.ensemble import RandomForestClassifier, HistGradientBoostingClassifier
 from xgboost import XGBClassifier
+from catboost import CatBoostClassifier
+from lightgbm import LGBMClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import (
     accuracy_score,
@@ -15,18 +13,120 @@ from sklearn.metrics import (
 )
 import optuna
 import mlflow
-import mlflow.sklearn
-import mlflow.xgboost
 
 from src.utils.common import get_project_root
 from src.utils.logger import logging
 
 RANDOM_STATE = 42 
 
-# ------------------------------------ mlflow experiment setup ----------------------------------- #
-def _setup_mlflow(
-    experiment_name: str = "churn-training-pipeline",
+
+# -------------------------------------- hyperparmeter setup ------------------------------------- #
+def _tune_model(model_name, X, y):
+    """hyperparameter setup with optuna and cross-validation"""
+
+    def objective(trial):
+        match model_name:
+            case "randomforest":
+                params = {
+                    "n_estimators": trial.suggest_int("n_estimators", 200, 500),
+                    "max_depth": trial.suggest_int("max_depth", 5, 20),
+                    "random_state": RANDOM_STATE,
+                    "n_jobs": -1
+                }
+                model = RandomForestClassifier(**params)
+            case "xgboost":
+                params = {
+                    "n_estimators": trial.suggest_int("n_estimators", 300, 800),
+                    "learning_rate": trial.suggest_float("learning_rate", 0.01, 0.1),
+                    "max_depth": trial.suggest_int("max_depth", 3, 10),
+                    "subsample": trial.suggest_float("subsample", 0.6, 1.0),
+                    "random_state": RANDOM_STATE
+                }
+                model = XGBClassifier(**params)
+
+            case "catboost":
+                params = {
+                    "iterations": trial.suggest_int("iterations", 300, 800),
+                    "learning_rate": trial.suggest_float("learning_rate", 0.01, 0.1),
+                    "depth": trial.suggest_int("depth", 4, 10),
+                    "l2_leaf_reg": trial.suggest_float("l2_leaf_reg", 1, 10),
+                    "random_seed": RANDOM_STATE,
+                    "verbose": False
+                }
+                model = CatBoostClassifier(**params)
+
+            case "lightgbm":
+                params = {
+                    "n_estimators": trial.suggest_int("n_estimators", 300, 800),
+                    "learning_rate": trial.suggest_float("learning_rate", 0.01, 0.1),
+                    "num_leaves": trial.suggest_int("num_leaves", 20, 150),
+                    "max_depth": trial.suggest_int("max_depth", -1, 15),
+                    "random_state": RANDOM_STATE
+                }
+                model = LGBMClassifier(**params)
+
+            case "hgboost":
+                params = {
+                    "learning_rate": trial.suggest_float("learning_rate", 0.01, 0.1),
+                    "max_iter": trial.suggest_int("max_iter", 200, 500),
+                    "max_depth": trial.suggest_int("max_depth", 3, 10),
+                    "random_state": RANDOM_STATE
+                }
+                model = HistGradientBoostingClassifier(**params)
+                
+        scores = cross_val_score(model, X, y, cv=3, scoring="recall", n_jobs=-1)
+        return scores.mean()
+    
+    return objective
+
+
+# ------------------------------- evaluate model and log in mlflow ------------------------------- #
+def _evaluate_model(model, X_test, y_test, y_pred):
+    """Evaluate and log model metrics across ltv segments in mlflow"""
+    
+    y_prob = model.predict_proba(X_test)[:, 1]
+    y_pred = (y_prob >= 0.5).astype(int)
+    
+    eval_df = X_test.copy()
+    eval_df['target'] = y_test
+    eval_df['prediction'] = y_pred
+    
+    # Static Thresholds from Notebook (A5 Policy)
+    thresholds = {
+        'VIP': 4400,
+        'IMP': 2600,
+        'High': 1800,
+        'Medium': 800,
+    }
+
+    def assign_segment(ltv):
+        if ltv >= thresholds['VIP']: return 'VIP'
+        if ltv >= thresholds['IMP']: return 'IMP'
+        if ltv >= thresholds['High']: return 'High'
+        if ltv >= thresholds['Medium']: return 'Medium'
+        return 'Low'
+
+    eval_df['Segment'] = eval_df['Lifetime_Value'].apply(assign_segment)
+    
+    segment_metrics = {}
+    for seg in ['VIP', 'IMP', 'High', 'Medium', 'Low']:
+        seg_data = eval_df.query("Segment == @seg")
+        if not seg_data.empty:
+            recall = recall_score(seg_data['target'], seg_data['prediction'], zero_division=0)
+            segment_metrics[f"recall_{seg.lower()}"] = recall
+
+    mlflow.log_metrics(segment_metrics)
+    return segment_metrics
+
+
+def build_model(
+    train_df: pd.DataFrame, 
+    test_df: pd.DataFrame,
+    target: str = "Churned", 
+    models: list[str] | None = None
 ):
+    """Construct and track model tuning experiments."""
+    experiment_name = "churn-training-pipeline"
     project_root = get_project_root()
     artifact_root = f"file://{project_root}/mlflow/experiment_logs"
     tracking_db = f"sqlite:///{project_root}/mlflow/tracking.db"
@@ -41,52 +141,41 @@ def _setup_mlflow(
         )
 
     mlflow.set_experiment(experiment_name)
-
-
-# -------------------------------------- hyperparmeter setup ------------------------------------- #
-def _tune_model(X, y):
-    """hyperparameter setup using optuna and cross-validation"""
-    def xgboost_objective(trial):
-        xgboost_params = {
-                "n_estimators": trial.suggest_int("n_estimators", 300, 800),
-                "learning_rate": trial.suggest_float("learning_rate", 0.01, 0.2),
-                "max_depth": trial.suggest_int("max_depth", 3, 10),
-                "subsample": trial.suggest_float("subsample", 0.5, 1.0),
-                "colsample_bytree": trial.suggest_float("colsample_bytree", 0.5, 1.0),
-                "random_state": RANDOM_STATE,
-                "n_jobs": -1,
-                "eval_metric": "logloss"
-            }
-        model = XGBClassifier(**params)
-        scores = cross_val_score(model, X, y, cv=3, scoring="recall")
-        return scores.mean()
-    params = {"xgboost": {}
-        
-    }
-    
-    study = optuna.create_study(direction="maximize")
-    study.optimize(xgboost_objective, n_trials=20)
-    return study.best_params
-
-# ------------------------------- evaluate model and log in mlflow ------------------------------- #
-def _evaluate_model():
-    """evaluate and log model metrics in mlflow"""
-    return
-
-def build_model(
-    train_df: pd.DataFrame,
-    test_df: pd.DataFrame,
-    target: str = "Churned",
-):
-    """build model"""
-    _setup_mlflow()
     
     X_train, y_train = train_df.drop(columns=[target]), train_df[target]
     X_test, y_test = test_df.drop(columns=[target]), test_df[target]
     
-    results: list[dict[str, float | str]] = []
+    if not models:
+        models = ["randomforest", "xgboost", "hgboost", "catboost", "lightgbm"]
     
-    best_model  = None
-    best_recall = -1.0
-    
-    return
+    for model_name in models:
+        # Parent Run for the Model Type
+        with mlflow.start_run(run_name=model_name):
+            
+            # 1. Hyperparameter Tuning with bayesian optimization
+            logging.info(f"MODEL_TRAIN: Tuning {model_name}")
+            study = optuna.create_study(direction="maximize")
+            study.optimize(_tune_model(model_name, X_train, y_train), n_trials=15)
+            
+            # 2. Collect and Train model with Best Params
+            match model_name:
+                case "randomforest": final_model = RandomForestClassifier(**study.best_params)
+                case "xgboost": final_model = XGBClassifier(**study.best_params)
+                case "hgboost": final_model = HistGradientBoostingClassifier(**study.best_params)
+                case "catboost": final_model = CatBoostClassifier(**study.best_params, verbose=False)
+                case "lightgbm": final_model = LGBMClassifier(**study.best_params)
+            final_model.fit(X_train, y_train)
+            
+            # 3. Log model metrics
+            y_pred = final_model.predict(X_test)
+            y_prob = final_model.predict_proba(X_test)
+            
+            mlflow.log_metric("recall", recall_score(y_test, y_pred))
+            mlflow.log_metric("avg_precision", average_precision_score(y_test, y_pred))
+            mlflow.log_metric("roc-auc", roc_auc_score(y_test, y_prob))
+            mlflow.log_metric("accuracy", accuracy_score(y_test, y_prob))
+            _evaluate_model(final_model, X_test, y_test)
+            
+            # 4. Log best model and params
+            mlflow.log_params(study.best_params)
+            mlflow.sklearn.log_model(final_model, artifact_path="model")
